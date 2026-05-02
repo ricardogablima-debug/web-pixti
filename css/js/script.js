2466531ce4ebdf74bbdd3d7b26d45bfa408fe5e9
ricardogablima-debug <ricardo.gab.lima@gmail.com>
2026-05-02 09:44:51 -0300

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -3,6 +3,14 @@ const descripcion = document.getElementById("ciclo-descripcion");
 
 fases.forEach(fase => {
   fase.addEventListener("click", () => {
+
+    // reset visual
+    fases.forEach(f => f.classList.remove("activa"));
+
+    // activar la seleccionada
+    fase.classList.add("activa");
+
+    // cambiar texto
     descripcion.textContent = fase.getAttribute("data-text");
   });
 });
