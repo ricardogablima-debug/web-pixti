fb69ec73ea28272d909e3e9d5c14ab5f9fe470da
ricardogablima-debug <ricardo.gab.lima@gmail.com>
2026-05-02 11:11:44 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,3 +42,22 @@ function setLang(lang) {
     }
   });
 }
+const translations = {
+  es: {
+    hero_title: "Entrenamiento desde adentro hacia afuera"
+  },
+  en: {
+    hero_title: "Training from the inside out"
+  }
+};
+
+function setLang(lang) {
+  const elements = document.querySelectorAll("[data-key]");
+  
+  elements.forEach(el => {
+    const key = el.getAttribute("data-key");
+    if (translations[lang][key]) {
+      el.textContent = translations[lang][key];
+    }
+  });
+}
